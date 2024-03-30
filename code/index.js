221c6642c74e6e7dc7679d44b688eae1e9898606
Eshwar Chandra Bandi <85159906+Eshwar-Chandra18@users.noreply.github.com>
2024-03-30 12:29:53 -0400

    Create index.js

---
+++ code/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const { Firestore } = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+  projectId: "sp24-41200-eshwar-globaljags"
+});
+
+exports.helloFirestore = async (event, context) => {
+  const resource = context.resource;
+  console.log('Function triggered by change to: ' +  resource);
+  const locations = event.value.fields.location?.arrayValue.values.map(value => value.stringValue);
+  const headline = event.value.fields.headline.stringValue;
+  console.log(locations);
+
+  let subscribersQuery = firestore.collection('subscribers'); 
+  let subscribers = await subscribersQuery.get();
+  
+  subscribers.forEach(subscriberDoc => {
+    const subscriberData = subscriberDoc.data();
+    const watchRegions = subscriberData.watch_regions;
+
+    for (let i = 0; i < watchRegions.length; i++) {
+      for (let j = 0; j < locations.length; j++) {
+        if (watchRegions[i] === locations[j]) {
+
+          const email = subscriberData.email_address;
+          console.log("Matched Location - ", locations[j], "For email ID - ", email);
+          const message = {
+            to: email,
+            from: "ebandi@iu.edu",
+            subject: `Eshwar Chandra Bandi ${headline}`,
+            text: `Location - ${locations[j]} Deal - ${headline}`
+          };
+
+          sgMail.send(message);
+          console.log("Email sent!");
+
+        }
+      }
+    }
+  });
+};
